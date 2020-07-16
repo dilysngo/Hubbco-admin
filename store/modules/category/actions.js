b7c5cbd2c1cb5dbeb1b9f0a73417da322a735f1c
giggs bui <giggs@namtech.com.au>
2020-07-16 18:59:23 +0700

    add vuex for list categories

--- store/modules/category/actions.js
+++ store/modules/category/actions.js
@@ -5,6 +5,8 @@ export default{
         if(!conditions)
             conditions = {};
         const data = await this.$axios.$get(`/api/categories/?keyword=${conditions.keyword || ''}&skip=${conditions.skip || 0}&limit=${conditions.limit || 10}`);
+        commit(types.CATEGORY_LISTCATE, data.results);
+        commit(types.CATEGORY_PAGINATION, data.pagination);
         return data;
     },
 
