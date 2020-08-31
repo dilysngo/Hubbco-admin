d310d7c7212ebbff90419afc8fd658ec41962112
Henry <henry@namtech.sg>
2020-08-31 17:37:07 +0700

    update

--- store/index.js
+++ store/index.js
@@ -34,7 +34,7 @@ export default function() {
             blog,
             provider,
             brands,
-            products:productsModule
+            // products:productsModule
         }
     });
 };
